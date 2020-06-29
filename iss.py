6243ec36f6ae12ce13532c954703e39db5cf1bc0
Drew Radcliff <sirampr@gmail.com>
2020-06-28 22:08:06 -0400

    GET coordinates

--- iss.py
+++ iss.py
@@ -10,12 +10,18 @@ def get_astronauts():
     return r.json()
 
 
+def get_coordinates():
+    r = requests.get('http://api.open-notify.org/iss-now.json')
+    return r.json()
+
+
 def main():
     data = get_astronauts()
     print(
         f"Number of astronauts on the {data['people'][0]['craft']}: {data['number']}")
     for astro in data["people"]:
         print(f"- {astro['name']}")
+    print(get_coordinates())
 
 
 if __name__ == '__main__':
