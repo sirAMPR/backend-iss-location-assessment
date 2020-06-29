8ebb996159ef2697f1c4f297d30befe1a3cad8ec
Drew Radcliff <sirampr@gmail.com>
2020-06-28 22:06:07 -0400

    GET astronaut data

--- iss.py
+++ iss.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+__author__ = 'aradcliff'
+
+import requests
+
+
+def get_astronauts():
+    r = requests.get('http://api.open-notify.org/astros.json')
+    return r.json()
 
 
 def main():
-    pass
+    data = get_astronauts()
+    print(
+        f"Number of astronauts on the {data['people'][0]['craft']}: {data['number']}")
+    for astro in data["people"]:
+        print(f"- {astro['name']}")
 
 
 if __name__ == '__main__':
