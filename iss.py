7b449abf075b16835711a5de6368c9ede786544d
Drew Radcliff <sirampr@gmail.com>
2020-06-28 23:57:01 -0400

    add next pass over indianapolis

--- iss.py
+++ iss.py
@@ -4,6 +4,7 @@ __author__ = 'aradcliff'
 
 import requests
 import turtle
+import time
 
 
 def get_astronauts():
@@ -16,15 +17,30 @@ def get_coordinates():
     return r.json()
 
 
+def get_next_pass():
+    r = requests.get('http://api.open-notify.org/iss-pass.json',
+                     params={'lat': '39.7684', 'lon': '-86.1581', 'n': '1'})
+    return r.json()
+
+
 def init_turtle():
     iss = turtle.Turtle()
+    next_pass = get_next_pass()
+    rise_time = time.ctime(next_pass["response"][0]["risetime"])
+
     screen = turtle.Screen()
     screen.setup(width=720, height=360, startx=0, starty=0)
     screen.setworldcoordinates(-180, -90, 180, 90)
     screen.bgpic('map.gif')
     screen.register_shape('iss.gif')
+
     iss.shape("iss.gif")
     iss.penup()
+    iss.goto(-86.1581, 39.7684)
+    iss.dot(5, "yellow")
+    iss.color('yellow')
+    iss.write(rise_time, align='right', font=("Arial", 14))
+
     return iss
 
 
@@ -43,9 +59,7 @@ def main():
         heading = iss.towards(lon, lat)
         if heading > 0.0:
             iss.setheading(heading)
-        print(iss.heading())
         iss.goto(lon, lat)
-        # print(coords)
 
 
 if __name__ == '__main__':
